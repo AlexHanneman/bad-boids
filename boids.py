a5037d26180df55722d7f9b929f8a44182e47fd6
Alex Hanneman <alexhanneman@hotmail.co.uk>
2022-01-27 16:23:10 +0000

    Refactored "flock" operations into Boids class

--- boids.py
+++ boids.py
@@ -9,80 +9,81 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-num_boids = 50
-middle_strength = 0.01/num_boids
-proximity_dist = 100
-match_vel_dist = 10000
-match_vel_strength = 0.125/num_boids
 
+class Boids:
+    def __init__(self, numBoids, flockStrength, proxDist, flockDist, matchVelStrength) -> None:
+        self.numBoids = numBoids
+        self.flockStrength = flockStrength
+        self.proxDist = proxDist
+        self.flockDist = flockDist
+        self.matchVelStrength = matchVelStrength
 
-def init_boids():
-    boids_x = [random.uniform(-450, 50.0) for x in range(num_boids)]
-    boids_y = [random.uniform(300.0, 600.0) for x in range(num_boids)]
-    boid_x_velocities = [random.uniform(0, 10.0) for x in range(num_boids)]
-    boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(num_boids)]
-    boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-    return boids
+    def init_boids(self):
+        self.xs = [random.uniform(-450, 50.0) for x in range(self.numBoids)]
+        self.ys = [random.uniform(300.0, 600.0) for x in range(self.numBoids)]
+        self.xvs = [random.uniform(0, 10.0) for x in range(self.numBoids)]
+        self.yvs = [random.uniform(-20.0, 20.0) for x in range(self.numBoids)]
+        return boids
 
+    def init_boids_from_file(self, fileData):
+        self.xs, self.ys, self.xvs, self.yvs = fileData
 
-def calc_boid_vel(x_a, y_a, xv_a, yv_a, x_b, y_b, xv_b, yv_b):
-    xVelChange = 0.0
-    yVelChange = 0.0
-
-    xSep = x_b - x_a
-    ySep = y_b - y_a
-
-    xVelChange += xSep*middle_strength
-    yVelChange += ySep*middle_strength
-
-    # Fly away from nearby boids
-    if check_distance(x_a, x_b, y_a, y_b, proximity_dist):
-        xVelChange -= xSep
-        yVelChange -= ySep
-
-    # Try to match speed with nearby boids
-    if check_distance(x_a, x_b, y_a, y_b, match_vel_dist):
-        xVelChange += (xv_b - xv_a) * match_vel_strength
-        yVelChange += (yv_b - yv_a) * match_vel_strength
-
-    return xVelChange, yVelChange
-
-
-def update_boids(boids):
-    xs, ys, xvs, yvs = boids
-    # Fly towards the middle
-    for i in range(len(xs)):
+    def calc_boid_vel(self, x_a, y_a, xv_a, yv_a, x_b, y_b, xv_b, yv_b):
         xVelChange = 0.0
         yVelChange = 0.0
-        for j in range(len(xs)):
-            velChange = calc_boid_vel(
-                xs[i], ys[i], xvs[i], yvs[i], xs[j], ys[j], xvs[j], yvs[j])
-            xVelChange += velChange[0]
-            yVelChange += velChange[1]
 
-        # Update velocity
-        xvs[i] += xVelChange
-        yvs[i] += yVelChange
+        xSep = x_b - x_a
+        ySep = y_b - y_a
 
-        # Move according to velocities
-        xs[i] = xs[i]+xvs[i]
-        ys[i] = ys[i]+yvs[i]
+        xVelChange += xSep*self.flockStrength
+        yVelChange += ySep*self.flockStrength
+
+        # Fly away from nearby boids
+        if self.check_distance(x_a, x_b, y_a, y_b, self.proxDist):
+            xVelChange -= xSep
+            yVelChange -= ySep
+
+        # Try to match speed with nearby boids
+        if self.check_distance(x_a, x_b, y_a, y_b, self.flockDist):
+            xVelChange += (xv_b - xv_a) * self.matchVelStrength
+            yVelChange += (yv_b - yv_a) * self.matchVelStrength
+
+        return xVelChange, yVelChange
+
+    def update_boids(self):
+        # Fly towards the middle
+        for i in range(self.numBoids):
+            xVelChange = 0.0
+            yVelChange = 0.0
+            for j in range(self.numBoids):
+                velChange = self.calc_boid_vel(
+                    self.xs[i], self.ys[i], self.xvs[i], self.yvs[i], self.xs[j], self.ys[j], self.xvs[j], self.yvs[j])
+                xVelChange += velChange[0]
+                yVelChange += velChange[1]
+
+            # Update velocity
+            self.xvs[i] += xVelChange
+            self.yvs[i] += yVelChange
+
+            # Move according to velocities
+            self.xs[i] += self.xvs[i]
+            self.ys[i] += self.yvs[i]
+
+    def check_distance(self, src_x, dst_x, src_y, dst_y, dist):
+        return (((dst_x-src_x)**2 + (dst_y-src_y)**2) < dist)
 
 
-def check_distance(src_x, dst_x, src_y, dst_y, dist):
-    return (((dst_x-src_x)**2 + (dst_y-src_y)**2) < dist)
-
-
-boids = init_boids()
+boids = Boids(50, 0.01/50, 100, 10000, 0.125/50)
+boids.init_boids()
 
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
+scatter = axes.scatter(boids.xs, boids.ys)
 
 
 def animate(frame):
-    update_boids(boids)
-    scatter.set_offsets(list(zip(boids[0], boids[1])))
+    boids.update_boids()
+    scatter.set_offsets(list(zip(boids.xs, boids.ys)))
 
 
 anim = animation.FuncAnimation(figure, animate,
--- test_boids.py
+++ test_boids.py
@@ -1,13 +1,17 @@
-from boids import update_boids
+import boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
 
+
 def test_bad_boids_regression():
-    regression_data=yaml.safe_load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
-    update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
-	
+    regression_data = yaml.safe_load(
+        open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+    boid_data = regression_data["before"]
+    boidsTest = boids.Boids(50, 0.01/50, 100, 10000, 0.125/50)
+    boidsTest.init_boids_from_file(boid_data)
+    boidsTest.update_boids()
+    checkData = (boidsTest.xs, boidsTest.ys, boidsTest.xvs, boidsTest.yvs)
+    for after, before in zip(regression_data["after"], checkData):
+        for after_value, before_value in zip(after, before):
+            assert_almost_equal(after_value, before_value, delta=0.01)
